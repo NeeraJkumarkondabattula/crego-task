01e172b5523633cd106e9ff3b19564aad2759abd
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2024-01-24 22:06:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpressionForm from "./component/ExpressionForm";
+
+function App() {
+  return (
+    <div className="pb-5 pt-3 bg-dark">
+      <h2 className="text-warning text-center mt-4 mb-5">Expression Form</h2>
+      <ExpressionForm />
+    </div>
+  );
+}
+
+export default App;
